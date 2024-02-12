21aa107c631f3b9cc1d2ccee75cef2e53524f55b
cucaracha <cucaracha@mail.ru>
2024-02-12 14:18:39 +0300

    PMA on-off option

--- updater.sh
+++ updater.sh
@@ -13,3 +13,6 @@ make project.user PROJECT=${PROJECT} CMD="yarn build"
 make project.user PROJECT=${PROJECT} CMD="cd public; yarn install; rm -r vendorjs; mv node_modules vendorjs;"
 
 #mysql -h localhost --protocol=tcp -u root -p ithub_ru < database.sql
+
+# Apply single container changes
+# https://stackoverflow.com/questions/31466428/how-to-restart-a-single-container-with-docker-compose/57185184#57185184
