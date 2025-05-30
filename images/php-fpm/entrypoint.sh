5bdad20af0f31e3ff4d0234d4823df925e18a782
cucaracha <cucaracha@mail.ru>
2025-05-30 14:13:27 +0300

    split add user and group actions

--- images/php-fpm/entrypoint.sh
+++ images/php-fpm/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "gid: $GID ${GID}"
-getent group ${GID} || (groupadd --gid ${GID} g${GID} && useradd --shell /bin/bash --uid ${UID} --gid ${GID} -m u${UID})
+echo "uid-gid: $UID:${GID}\n"
+getent group ${GID} || groupadd --gid ${GID} g${GID}
+getent passwd ${UID} || useradd --shell /bin/bash --uid ${UID} --gid ${GID} -m u${UID}
 
 /sbin/my_init
